dd4f179c666d760089344a40dc6a4f6ceee97d8f
andrekiba <andre.kiba@gmail.com>
2016-03-15 18:13:17 +0100

    update

--- SkiaElfo/SkiaElfo/Pages/ElfoLogoPage.cs
+++ SkiaElfo/SkiaElfo/Pages/ElfoLogoPage.cs
@@ -17,18 +17,19 @@ namespace SkiaElfo.Pages
         public static void DrawElfoLogo(SKCanvas canvas, int width, int height)
         {
             var totalSize = ((float)height > width ? width : height) * 0.5f;
-            var center = new SKPoint((width - totalSize) / 2f, (height - totalSize) / 2f);
             var squareSize = totalSize / 3f;
-            var bottomLeft = new SKPoint((width - totalSize) / 2f, (height - totalSize) / 2f);
+            var topLeft = new SKPoint((width - totalSize) / 2f, (height - totalSize) / 2f);
 
+            var blackSquare1 = SKRect.Create(topLeft.X, topLeft.Y, squareSize, squareSize);
+            var blackSquare2 = SKRect.Create(topLeft.X + squareSize, topLeft.Y, squareSize, squareSize);
+            var blackSquare3 = SKRect.Create(topLeft.X + (squareSize * 2), topLeft.Y, squareSize, squareSize);
+            var blackSquare4 = SKRect.Create(topLeft.X, topLeft.Y + squareSize, squareSize, squareSize);
+            var blackSquare5 = SKRect.Create(topLeft.X + squareSize, topLeft.Y + squareSize, squareSize, squareSize);
+            var blackSquare6 = SKRect.Create(topLeft.X + (squareSize * 2), topLeft.Y + squareSize, squareSize, squareSize);
 
-
-            var blackSquare1 = SKRect.Create(center.X - totalSize / 2f, center.Y, squareSize, squareSize);
-            //var blackSquare2 = SKRect.Create(center.X - size / 2f, center.Y, squareSize, squareSize);
-            //var blackSquare3 = SKRect.Create(center.X - size / 2f, center.Y, squareSize, squareSize);
-            //var blackSquare4 = SKRect.Create(center.X - size / 2f, center.Y, squareSize, squareSize);
-            //var blackSquare5 = SKRect.Create(center.X - size / 2f, center.Y, squareSize, squareSize);
-            //var blackSquare6 = SKRect.Create(center.X - size / 2f, center.Y, squareSize, squareSize);
+            var whiteSquare1 = SKRect.Create(topLeft.X, topLeft.Y + (squareSize * 2), squareSize, squareSize);
+            var whiteSquare2 = SKRect.Create(topLeft.X + squareSize, topLeft.Y + (squareSize * 2), squareSize, squareSize);
+            var whiteSquare3 = SKRect.Create(topLeft.X + (squareSize * 2), topLeft.Y + (squareSize * 2), squareSize, squareSize);
 
             //var rightRect = SKRect.Create(center.X + size / 2f, center.Y, size, size);
 
@@ -38,22 +39,31 @@ namespace SkiaElfo.Pages
             using (var paint = new SKPaint())
             {
                 paint.IsAntialias = true;
-                canvas.Clear(SKColors.White);
+                canvas.Clear(SKColors.BlueViolet);
 
                 paint.Color = SKColors.Black;
                 
                 // black1
                 canvas.DrawRect(blackSquare1, paint);
                 // black2
-                //canvas.DrawRect(blackSquare2, paint);
+                canvas.DrawRect(blackSquare2, paint);
                 // black3
-                //canvas.DrawRect(blackSquare3, paint);
+                canvas.DrawRect(blackSquare3, paint);
                 // black4
-                //canvas.DrawRect(blackSquare4, paint);
+                canvas.DrawRect(blackSquare4, paint);
                 // black5
-                //canvas.DrawRect(blackSquare5, paint);
+                canvas.DrawRect(blackSquare5, paint);
                 // black6
-                //canvas.DrawRect(blackSquare6, paint);
+                canvas.DrawRect(blackSquare6, paint);
+
+                paint.Color = SKColors.White;
+
+                // white1
+                canvas.DrawRect(whiteSquare1, paint);
+                // white2
+                canvas.DrawRect(whiteSquare2, paint);
+                // white3
+                canvas.DrawRect(whiteSquare3, paint);
 
                 // save
                 canvas.Save();
